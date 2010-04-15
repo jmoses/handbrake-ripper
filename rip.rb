02d4296bce1d180bc764d3a471fe11fb722a7da6
Jon Moses <jon@burningbush.us>
2010-04-15 09:54:24 -0400

    Lots of changes.  Now automatic mode that will scan the DVD, pick the best title and audio track, query IMDB and go to town.  Start of support for deteting foreign lang dvds, but no action taken, just warning.

--- rip.rb
+++ rip.rb
@@ -6,21 +6,153 @@ require 'tempfile'
 require 'hirb'
 require 'hpricot'
 require 'open-uri'
+require 'fileutils'
+require 'open3'
+
+class HandBrake
+  class Title
+    attr_accessor :title_id
+    attr_accessor :audio_tracks
+    attr_accessor :subtitles
+    attr :duration, true
+        
+    BEST_AUDIO_FORMAT = 'AC3'
+    BEST_AUDIO_SUBFORMAT = /5\.1/
+    BEST_AUDIO_LANGUAGE = 'English'
+        
+    def initialize( tid )
+      @title_id = tid
+      @audio_tracks = []
+      @duration = 0
+      @subtitles = []
+    end
+    
+    def best_audio
+      @best_audio_obj ||= audio_tracks.detect {|t| t.format == BEST_AUDIO_FORMAT and t.subformat =~ BEST_AUDIO_SUBFORMAT and t.language == BEST_AUDIO_LANGUAGE}
+      
+      if @best_audio_obj.track_id != '1'
+        STDERR.puts("Best audio track is not track 1!  Possible foreign language film detected!")
+      end
+      @best_audio_obj
+    end
+    
+    def duration_window
+      ((duration - (60 * 15))..(duration + (60 * 15)))
+    end
+    
+    def arguments
+      "--title #{title_id} #{best_audio.arguments}"
+    end
+  end
+  
+  class AudioTrack
+    attr_accessor :track_id
+    attr_accessor :format
+    attr_accessor :subformat
+    attr_accessor :language
+    
+    def initialize( tk_id, language, format, subformat )
+      @track_id = tk_id
+      @format = format
+      @language = language
+      @subformat = subformat
+    end
+    
+    def arguments
+      "--audio #{track_id}"
+    end
+  end
+  
+  class Subtitle
+    attr_accessor :subtitle_id
+    attr_accessor :language
+    
+    def initialize( sid, lang )
+      @subtitle_id = sid
+      @language = lang
+    end
+  end
+  
+  attr_accessor :device
+  attr_accessor :titles
+  
+  def initialize( options = {})
+    options.each do |k,v|
+      instance_variable_set("@#{k}", v)
+    end
+    @titles = []
+  end
+  
+  def scan_titles
+    current_title = nil
+    handbrake("-t 0").each do |line|
+      case line
+      when /^\+ title (\d+)\:/
+        @titles << Title.new($1)
+        current_title = @titles.last
+      when /\+ duration: (\d{2})\:(\d{2})\:(\d{2})/
+        current_title.duration = ( $1.to_i * 3600 ) + ( $2.to_i * 60 ) + ( $3.to_i )
+      when /\+ (\d), ([a-zA-Z]+) \(([a-zA-Z0-9]+)\) \((.*?)\), \d+Hz, \d+bps/
+        current_title.audio_tracks << AudioTrack.new($1, $2, $3, $4)
+      when /\+ (\d), ([a-zA-Z]+).*?(\(iso[\d\-]+: [a-z]+)\)/
+        current_title.subtitles << Subtitle.new($1, $2)
+      end
+    end
+  end
+  
+  def best_title
+    return @best_title_obj if @best_title_obj
+    
+    @best_title_obj ||= titles.sort {|a,b| a.duration <=> b.duration }.reverse.first
+    
+    # if any within 15 minutes, alert
+    if titles.reject {|t| t.title_id == @best_title_obj.title_id }.any? {|t| @best_title_obj.duration_window.include?(t.duration) }
+      STDERR.puts "Multiple possibile titles based on duration!  Using longest title: #{@best_title_obj.title_id}"
+    end
+
+    @best_title_obj
+  end
+  
+  def has_real_best?
+    best_title and best_title.best_audio
+  end
+  
+  def base_command
+    "HandBrakeCLI -i #{device} "
+  end
+  
+  def best_arguments
+    has_real_best? ? best_title.arguments : nil
+  end
+  
+  def handbrake( commands )
+    Open3.popen3("#{base_command} #{commands}") do |stdin, stdout, stderr|
+      stderr.readlines
+    end
+  end
+end
 
 
-output_path = "/mnt/MISC/Movies/"
 
 opts = Trollop::options do 
 	opt :title, "Which title to rip", :type => :integers
 	opt :subtitle, "Which subtitle to use (ie: eng)", :default => ''
 	opt :audio, "Which audio track to rip", :default => 1
 	opt :preset, 'Which preset to use', :default => 'film'
-	opt :search, "Search IMDB for title details", :default => false
+  # opt :search, "Search IMDB for title details", :default => false
+  opt :output, "Filename to output to", :type => :string
 	opt :pretend, "Just output the script to execute", :default => false
+	opt :device, "Device to use", :default => '/dev/scd0'
+	opt :output_path, "Path to output files to", :default => '/mnt/MISC/Movies/'
+	opt :auto, "Do everything automatically", :default => false, :short => "A"
 end
 
-output = ARGV
+output_path = opts[:output_path]
 
+unless output_path and File.exists?(output_path) and File.directory?(output_path)
+  STDERR.puts("Output path (#{output_path}) is not valid.")
+  exit 1 unless opts[:pretend]
+end
 
 preset = case opts[:preset]
 when 'animation'
@@ -33,10 +165,10 @@ end
 
 titles = ( opts[:title] or [1] )
 
-filenames = output
+filenames = []
 
-if opts[:search]
-  doc = Hpricot( open("http://www.imdb.com/find?s=all&q=#{filenames.join('+')}") {|f| f.read } )
+if ! opts[:output]
+  doc = Hpricot( open("http://www.imdb.com/find?s=all&q=#{ARGV.join('+')}") {|f| f.read } )
   
   possible_titles = []
   
@@ -45,9 +177,15 @@ if opts[:search]
       possible_titles << (row/'td').last.inner_text.match(/(^.*?\))/)[0]
     end
   end
-  filenames = Hirb::Menu.render possible_titles, :helper_class => false
+  if possible_titles.size == 1 and opts[:auto]
+    filenames = possible_titles
+  else
+    filenames = Hirb::Menu.render possible_titles, :helper_class => false
+  end
   
   filenames.each {|f| f.gsub!(/[:\/]/, '-') }
+else
+  filenames = ARGV
 end
 
 if filenames.size != titles.size
@@ -59,15 +197,35 @@ if filenames.size != titles.size
 	end
 end
 
-cmd = %Q{HandBrakeCLI #{preset}  -t %d -a #{opts[:audio]} #{opts[:subtitle] != '' ? "-N #{opts[:subtitle]}" : ''} -i /dev/scd0 -m -o /mnt/MISC/"%s.mkv"}
+
+
+cmd = %Q{HandBrakeCLI #{preset} -i #{opts[:device]} -m }
+manual_cmd = %Q{ -t %d -a #{opts[:audio]} #{opts[:subtitle] != '' ? "-N #{opts[:subtitle]}" : ''} -o #{output_path}/"%s.mkv"}
+
 
 cmds = []
 
-titles.zip(filenames).each do |title, filename|
-	filename = filename.gsub('%title%', sprintf('%02d', title.to_i) )
-	cmds << sprintf(cmd, title, filename)
+if opts[:auto]
+  if filenames.size != 1
+    STDERR.puts("Only one filename allowed when auto ripping.")
+    exit 1
+  end
+  
+  hb = HandBrake.new( :device => opts[:device])
+  hb.scan_titles
+  
+  if hb.has_real_best?
+    cmds << cmd + hb.best_arguments + %Q{ -o #{output_path}/"#{filenames.first}.mkv"}
+  else
+    STDERR.puts "No best title found, please run manually."
+    exit 1
+  end
+else
+  titles.zip(filenames).each do |title, filename|
+  	filename = filename.gsub('%title%', sprintf('%02d', title.to_i) )
+  	cmds << sprintf(cmd + manual_cmd, title, filename)
+  end
 end
-
 
 tf = Tempfile.new("rip")
 
@@ -80,5 +238,15 @@ tf.rewind
 if opts[:pretend]
   exec "cat #{tf.path} && rm #{tf.path}"
 else
+  puts File.read(tf.path)
+  puts
+  STDOUT.sync = true
+  STDOUT.write "Running the above command in "
+  begin
+    5.downto(1) {|i| STDOUT.write("#{i}..."); sleep 1}
+  rescue Interrupt
+    STDOUT.puts("\nCancelling.")
+    exit 0
+  end
   exec "sh #{tf.path} && rm #{tf.path}"
 end
